45ee4e476c4ca12ba7398620f3786efcad4e148b
Ramon Kerpershoek <ramon.kerpershoek@gmail.com>
2017-05-10 14:11:12 +0200

    design changes

--- build/navbar.php
+++ build/navbar.php
@@ -11,7 +11,7 @@ if(empty($_SESSION['user']['role'])) {
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-            <a class="navbar-brand">TZT</a>
+            <a class="navbar-brand" href="index.php">TZT</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
@@ -19,9 +19,11 @@ if(empty($_SESSION['user']['role'])) {
                 <li <?php if($file == 'index'){?> class="active" <?php } ?>><a href="index.php">Home</a></li>
                 <li <?php if($file == 'info'){?> class="active" <?php } ?>><a href="info.php">Info</a></li>
                 <li <?php if($file == 'test'){?> class="active" <?php } ?>><a href="index.php">Home</a></li>
-                <li <?php if($file == 'test'){?> class="active" <?php } ?>><a href="index.php">Home</a></li>
-                
-            </ul>           
+                <li <?php if($file == 'test'){?> class="active" <?php } ?>><a href="index.php">Home</a></li>                
+            </ul> 
+            <ul class="nav navbar-nav pull-right">  
+            	<li class="active"><a class="active" href="login.php">login</a></li>
+            </ul>        
         </div>
     </div>
  </nav>
--- index.php
+++ index.php
@@ -3,14 +3,31 @@ include("includes/autoloader.php");
 include("build/head.php");
 include("build/navbar.php");
 ?>
-<div class="row">
-	<div class="col-xs-6">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex enim, ultricies ac nibh non, porta porta purus. Integer sodales lacinia ex, quis hendrerit eros tempor at. Sed dui sem, interdum vitae ultricies faucibus, pellentesque ut ante. Maecenas tempus enim ornare tortor imperdiet tempor. Suspendisse lectus velit, interdum at sapien quis, finibus pulvinar enim. Nunc massa justo, euismod eget semper eget, elementum id elit. Sed suscipit orci augue, in auctor est feugiat sit amet. Vestibulum non magna molestie, auctor risus in, tempor metus. Nulla et libero ac libero elementum lacinia vitae sed nunc. Morbi enim turpis, interdum pharetra pellentesque non, tempus sit amet sapien. Pellentesque lacus ante, luctus eu diam eget, dapibus consequat turpis. Nullam eget est elementum, convallis arcu id, pharetra mauris. Curabitur lorem leo, tempus sed arcu et, bibendum porta orci. Nam maximus sapien nec elementum placerat. Fusce pellentesque arcu ac lacus hendrerit ultrices. Vivamus rhoncus, nulla sit amet iaculis rhoncus, augue magna tincidunt orci, ut posuere arcu libero non nibh.</p>
+
+	<div class="row no-gutters">
+		<div class="col-xs-12 col-md-6 padding-top box-column2">
+			<h1>TZT AWESOME!!!</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex enim, ultricies ac nibh non, porta porta purus. Integer sodales lacinia ex, quis hendrerit eros tempor at. Sed dui sem, interdum vitae ultricies faucibus, pellentesque ut ante. Maecenas tempus enim ornare tortor imperdiet tempor. Suspendisse lectus velit, interdum at sapien quis, finibus pulvinar enim. Nunc massa justo, euismod eget semper eget, elementum id elit. Sed suscipit orci augue, in auctor est feugiat sit amet. Vestibulum non magna molestie, auctor risus in, tempor metus. </p>
+		</div>
+		<div class="col-xs-12 col-md-6 padding-0 box-column2">
+			<img class="img-responsive" src="images/trein.jpg">
+		</div			
 	</div>
-	<div class="col-xs-6">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex enim, ultricies ac nibh non, porta porta purus. Integer sodales lacinia ex, quis hendrerit eros tempor at. Sed dui sem, interdum vitae ultricies faucibus, pellentesque ut ante. Maecenas tempus enim ornare tortor imperdiet tempor. Suspendisse lectus velit, interdum at sapien quis, finibus pulvinar enim. Nunc massa justo, euismod eget semper eget, elementum id elit. Sed suscipit orci augue, in auctor est feugiat sit amet. Vestibulum non magna molestie, auctor risus in, tempor metus. Nulla et libero ac libero elementum lacinia vitae sed nunc. Morbi enim turpis, interdum pharetra pellentesque non, tempus sit amet sapien. Pellentesque lacus ante, luctus eu diam eget, dapibus consequat turpis. Nullam eget est elementum, convallis arcu id, pharetra mauris. Curabitur lorem leo, tempus sed arcu et, bibendum porta orci. Nam maximus sapien nec elementum placerat. Fusce pellentesque arcu ac lacus hendrerit ultrices. Vivamus rhoncus, nulla sit amet iaculis rhoncus, augue magna tincidunt orci, ut posuere arcu libero non nibh.</p>
+	
+	<div class="container-fluid padding-0">
+		<div class="row">
+			<div class="col-xs-12 col-md-4 box-column padding-0">
+				<p class="text-center specialtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			</div>
+			<div class="col-xs-12 col-md-4 box-column padding-0">
+				<p class="text-center specialtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			</div>
+			<div class="col-xs-12 col-md-4 box-column padding-0">
+				<button class="btn btn-primary center-block" type="button">Lorem</button>
+			</div>
+		</div>
 	</div>
-</div>;
+
 <?php
 include("build/footer.php");
 ?>
