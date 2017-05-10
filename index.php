068bfd37b1b0464f5a24b217aa95dd232ce6eb95
Ramon Kerpershoek <ramon.kerpershoek@gmail.com>
2017-05-10 16:29:39 +0200

    changes

--- index.php
+++ index.php
@@ -3,27 +3,38 @@ include("includes/autoloader.php");
 include("build/head.php");
 include("build/navbar.php");
 ?>
-
-	<div class="row no-gutters">
-		<div class="col-xs-12 col-md-6 padding-top box-column2">
-			<h1>TZT AWESOME!!!</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex enim, ultricies ac nibh non, porta porta purus. Integer sodales lacinia ex, quis hendrerit eros tempor at. Sed dui sem, interdum vitae ultricies faucibus, pellentesque ut ante. Maecenas tempus enim ornare tortor imperdiet tempor. Suspendisse lectus velit, interdum at sapien quis, finibus pulvinar enim. Nunc massa justo, euismod eget semper eget, elementum id elit. Sed suscipit orci augue, in auctor est feugiat sit amet. Vestibulum non magna molestie, auctor risus in, tempor metus. </p>
-		</div>
-		<div class="col-xs-12 col-md-6 padding-0 box-column2">
-			<img class="img-responsive" src="images/trein.jpg">
-		</div			
-	</div>
-	
-	<div class="container-fluid padding-0">
+	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-md-4 box-column padding-0">
-				<p class="text-center specialtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			<div class="col-xs-12 col-md-6 padding-auto" >
+				<div class="col-md-12 lightgrey-background padding-auto">
+					<h1>TZT AWESOME!!!</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ex enim, ultricies ac nibh non, porta porta purus. Integer sodales lacinia ex, quis hendrerit eros tempor at. Sed dui sem, interdum vitae ultricies faucibus, pellentesque ut ante. Maecenas tempus enim ornare tortor imperdiet tempor. Suspendisse lectus velit, interdum at sapien quis, finibus pulvinar enim. Nunc massa justo, euismod eget semper eget, elementum id elit. Sed suscipit orci augue, in auctor est feugiat sit amet. Vestibulum non magna molestie, auctor risus in, tempor metus. </p>
+					<button class="btn btn-info btn-block" type="button">Schrijf je nu hier in!</button>
+				</div>
 			</div>
-			<div class="col-xs-12 col-md-4 box-column padding-0">
-				<p class="text-center specialtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			<div class="col-xs-12 col-md-6 padding-auto">
+				<div class="col-md-12 ">					
+					<img class="img-responsive" src="images/trein.jpg">
+				</div>
+			</div>			
+		</div>	
+	
+	
+		<div class="row top-buffer orange-background">
+			<div class="col-xs-12 col-md-4">
+				<div class="col-md-12">
+					<p class="text-center specialtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
 			</div>
-			<div class="col-xs-12 col-md-4 box-column padding-0">
-				<button class="btn btn-primary center-block" type="button">Lorem</button>
+			<div class="col-xs-12 col-md-4">
+				<div class="col-md-12">
+					<p class="text-center specialtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
+			</div>
+			<div class="col-xs-12 col-md-4">
+				<div class="col-md-12">
+					<p class="text-center specialtext">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>					
+				</div>
 			</div>
 		</div>
 	</div>
