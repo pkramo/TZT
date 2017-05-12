03cb36053d51248e6ba457635fe449cfd30db5f3
mightybundy <s1103684@student.windesheim.nl>
2017-05-12 16:26:27 +0200

    register

--- includes/controls/accountManagement.php
+++ includes/controls/accountManagement.php
@@ -3,106 +3,33 @@ class accountManagement
 {
 
 //Registreren gebruiker
-	public static function register()
-	{
-		$register  = new dataAccountManagement;
-		$username  = Input::get('username');
-		$password  = Input::get('password');
-		$fname     = Input::get('f_name');
-		$insertion = Input::get('insertion');
-		$lname     = Input::get('l_name');
-		$rol       = Input::get('rol');
-		$email     = Input::get('email');
-		$tel       = Input::get('tel');
-		$dys       = Input::get('dys');
-		$comment   = Input::get('comment');
-		$date      = Input::get('join_date');
-        $class     = Input::get('class');
-        $school    = Input::get('school');
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $check = true;
-       foreach (accountManagement::userInfo() as $user) {
-            if ($user['username'] == $username) {
-                $check = FALSE;
-            }
-            }
-        
-        if ($check) {
-        if(!empty(input::get("username"))) { // kijkt of gebruikersnaam niet leeg is
-          if(preg_match("/^[a-zA-Z ]*$/",$username)){ // kijkt of er geen rare tekens in gebruikersnaam staan
-            if(!empty(input::get("password"))) { // kijkt of wachtwoord leeg is
-                if (!empty(input::get("f_name"))){ // kijkt of voornaam leeg is
-                   if(preg_match("/^[a-zA-Z ]*$/",$fname)) { // kijkt of er geen rare tekens in de voornaam staan
-                     if(preg_match("/^[a-zA-Z ]*$/",$insertion)) { // kijkt of geen rare tekens bij tussenvoegsel staan
-                      if(!empty(input::get("l_name"))) { // kijkt of achternaam niet leeg is
-                        if(preg_match("/^[a-zA-Z ]*$/",$lname)) { // kijkt of er geen rare tekens bij achternaam staan
-                          if (preg_match("/^[a-zA-Z0-9 ]*$/",$class)) { // kijkt of er geen rare tekens bij klas staan
-                           if(preg_match("/^[a-zA-Z ]*$/",$school)) { // kijkt of er geen rare tekens bij school staan 
-                              if(filter_var($email, FILTER_VALIDATE_EMAIL) || empty(input::get("email")))  { // kijkt of email wel goed geformuleerd is
-                                //$_SESSION['alert'] = true; 
-                                //$_SESSION['message'] = '<div class="alert alert-danger">email is goed</div>';
-                                   if(preg_match("/^[0-9]*$/",$tel)) { // kijkt of telefoonnummer wel uit cijfers bestaan
-                                      if ($register->setRegister($username,$password,$fname,$insertion,$lname,$rol,$email,$tel,$dys,$comment,$date,$class,$school)) {
-			                                         $_SESSION['alert'] = true; 
-                                                    $_SESSION['message'] = '<div class="alert alert-success">Gebruiker succesvol opgeslagen!</div>';
-                                       // maakt alle variablen leeg nadat de gebruiker succesvol is opgeslagen
-                                       $_POST['username'] = $_POST['password'] = $_POST['f_name'] = $_POST['insertion'] = $_POST['l_name'] = $_POST['rol'] = $_POST['email'] = $_POST['tel'] = $_POST['dys'] = $_POST['comment'] = $_POST['join_date'] = $_POST['class'] = $_POST['school'] = "";}
-                                       
-                                   } else { // plaatst alert over verkeerd ingevuld
-                                    $_SESSION['alert'] = true; 
-                                $_SESSION['message'] = '<div class="alert alert-danger">telnr is verkeerd</div>'; 
-                                   }
-                               } else { // plaatst alert over verkeerd ingevuld
-                                 $_SESSION['alert'] = true; 
-                                $_SESSION['message'] = '<div class="alert alert-danger">email is verkeerd</div>';  
-                               } 
-                           } else { // plaatst alert over verkeerd ingevuld
-                    $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">School is verkeerd</div>';
-                           }  
-                          }  else { // plaatst alert over verkeerd ingevuld
-                               $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">klas is verkeerd</div>'; 
-                          }
-                        }  else { // plaatst alert over verkeerd ingevuld
-                             $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">Achternaam is verkeerd</div>';   
-                        }
-                      } else { // plaatst alert over het niet ingevulde veld
-                       $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">Achternaam is verplicht</div>';    
-                      }
-                     }  else { // plaatst alert over verkeerd ingevuld
-                       $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">Tussenvoegsel is verkeerd</div>';  
-                     }
-                   } else { // plaatst alert over verkeerd ingevuld
-                     $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">Voornaam is verkeerd</div>';  
-                   }
-                } else { // plaatst alert over het niet ingevulde veld
-                   $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">Voornaam is verplicht</div>'; 
-                }
-            }  else { // plaatst alert over het niet ingevulde veld
-              $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">Wachtwoord is verplicht</div>';  
-            }
-          } else { // plaatst alert over verkeerd ingevuld
-           $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">Gebruikersnaam is verkeerd</div>';   
-          }
-        } else { // plaatst alert over het niet ingevulde veld
-         $_SESSION['alert'] = true; 
-            $_SESSION['message'] = '<div class="alert alert-danger">Gebruikersnaam is verplicht</div>';   
+	public static function register(){
+            $register  = new dataAccountManagement;
+            $voornaam = input::get('Voornaam');
+            $voorletter = input::get('Voorletter');
+            $tussenvoegsel = input::get('Tussenvoegsel');
+            $achternaam = input::get('Achternaam');
+            $geboortedatum = input::get('Geboortedatum');
+            $straat = input::get('Straat');
+            $huisnummer = input::get('Huisnummer');
+            $postcode = input::get('Postcode');
+            $woonplaats = input::get('Woonplaats');
+            $telefoonnummer = input::get('Telefoonnummer');
+            $emailadres = input::get('Emailadres');
+            $wachtwoord = input::get('Wachtwoord');
+            $soortklant = input::get('Soort');
+                
+            $bedrijfsnaam = input::get('Bedrijfsnaam');
+            $KVK = input::get('KVK');
+            $straatbedrijf = input::get('Straatnaam bedrijf');
+            $huisnummerbedrijf = input::get('Huisnummer bedrijf');
+            $postcodebedrijf = input::get('Postcode bedrijf');
+            $plaatsbedrijf = input::get('Plaats bedrijf');
+            $telefoonnummerbedrijf = input::get('Telefoonnummer bedrijf');
+            VAR_DUMP($voornaam, $voorletter, $tussenvoegsel, $achternaam, $geboortedatum, $straat, $huisnummer, $postcode, $woonplaats, $telefoonnummer, $emailadres, $wachtwoord, $soortklant, $bedrijfsnaam, $KVK, $straatbedrijf, $huisnummerbedrijf, $postcodebedrijf, $plaatsbedrijf, $telefoonnummerbedrijf);
+            exit;
+            
         }
-        } else {
-             $_SESSION['alert'] = true;
-            $_SESSION['message'] = '<div class="alert alert-danger form-group">Gebruikersnaam is al in gebruik</div>';
-        }
-
-    }
-
 	
 	public static function login()
 	{
--- register.php
+++ register.php
@@ -16,6 +16,7 @@ include("build/navbar.php");
 					    <div class="form-group">
 						    <label for="Voornaam">Voornaam*</label> <input required class="form-control" type="text" name="Voornaam"> 
 						    <label for="Voorletter">Voorletter*</label> <input required class="form-control" type="text" name="Voorletter">
+                                                    <label for="Tussenvoegsel">Tussenvoegsel</label> <input required class="form-control" type="text" name="Tussenvoegsel"> 
 						    <label for="Achternaam">Achternaam* </label> <input required class="form-control" type="text" name="Achternaam">
 						    <label for="Geboortedatum">Geboortedatum* </label> <input required class="form-control" type="date" name="Geboortedatum">
 						    <label for="Straat">Straat*</label> <input required class="form-control" type="text" name="Straat">
@@ -24,9 +25,7 @@ include("build/navbar.php");
 						    <label for="Woonplaats">Woonplaats* </label> <input required class="form-control" type="text" name="Woonplaats">
 						    <label for="Telefoonnummer"> Telefoonnummer*</label><input required class="form-control" type="text" name="Telefoonnummer">
 						    <label for="Emailadres">Emailadres* </label><input required class="form-control" type="email" name="Emailadres">
-						    <label for="Herhaal Emailadres">Herhaal emailadres* </label> <input required class="form-control" type="email" name="Emailadres">
 						    <label for="Wachtwoord">Wachtwoord* </label> <input required class="form-control" type="password" name="Wachtwoord">
-						    <label for="Herhaal wachtwoord">Herhaal wachtwoord* </label> <input required class="form-control" type="password" name="Wachtwoord">
 						    <div class="form-group">
 							    <label for="Maak uw keuze">Maak uw keuze:* </label> 
 							    <div class="radio">
@@ -38,10 +37,10 @@ include("build/navbar.php");
 						    </div>
 						    <label for="Bedrijfsnaam">Bedrijfsnaam </label> <input class="form-control" type="text" name="Bedrijfsnaam">
 						    <label for="KVK-nummer">KVK-nummer</label> <input class="form-control" type="text" name="KVK">
-						    <label for="Plaats bedrijf">Plaats bedrijf</label> <input class="form-control" type="text" name="Plaats bedrijf">
+						    <label for="Straatnaam bedrijf">Straatnaam bedrijf</label> <input class="form-control" type="text" name="Straatnaam bedrijf">
+						    <label for="Huisnummer bedrijf">Huisnummer bedrijf</label> <input class="form-control" type="text" name="Huisnummer bedrijf">
 						    <label for="Postcode bedrijf">Postcode bedrijf</label> <input class="form-control" type="text" name="Postcode bedrijf">
-						    <label for="Woonplaats">Straatnaam bedrijf</label> <input class="form-control" type="text" name="Straatnaam bedrijf">
-						    <label for="Straatnummer bedrijf">Straatnummer bedrijf</label> <input class="form-control" type="text" name="Straatnummer bedrijf">
+						    <label for="Plaats bedrijf">Plaats bedrijf</label> <input class="form-control" type="text" name="Plaats bedrijf">
 						    <label for="Telefoonnummer bedrijf">Telefoonnummer bedrijf</label> <input class="form-control" type="text" name="Telefoonnummer bedrijf">
 						    <p>* = verplicht invullen</p>
 					    </div>
